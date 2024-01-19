467cad50a6c189a4e02bbd7f4a9eaff4f8f48c9d
Shun Isii <sisii00.swork@gmail.com>
2024-01-19 14:55:14 +0900

    ddForwarderとの紐付けをCDKでやっちゃおうようにした

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import aws_cdk as cdk
 from aws_cdk import aws_cloudfront as cloudfront
 from aws_cdk import aws_cloudfront_origins as origins
+from aws_cdk import aws_lambda as lambda_
 from aws_cdk import aws_s3 as s3
+from aws_cdk.aws_lambda_event_sources import S3EventSource
 
 app = cdk.App()
 stack = cdk.Stack(app, "cloud-front-error-metrics-stack")
@@ -23,9 +25,19 @@ log_bucket = s3.Bucket(
     auto_delete_objects=True,
 )
 
-cf_function = cloudfront.Function(stack, "Function", code=cloudfront.FunctionCode.from_file(file_path="function.js"))
+# DatadogForwarderをバケットへ紐付ける
+DD_FORWARDER_ARN = "arn:aws:lambda:ap-northeast-1:000000000000:function:xxxxxxxxxxxx"  # DatadogForwarderのARNを指定する
+dd_forwarder = lambda_.Function.from_function_arn(stack, "DDForwarder", function_arn=DD_FORWARDER_ARN)
+dd_forwarder.add_event_source(
+    S3EventSource(
+        bucket=log_bucket,
+        events=[s3.EventType.OBJECT_CREATED],
+        filters=[s3.NotificationKeyFilter(prefix="cloudfront")],  # CloudFrontのログのみをDatadogに転送する
+    )
+)
 
 # CloudFrontのディストリビューションを作成する
+cf_function = cloudfront.Function(stack, "Function", code=cloudfront.FunctionCode.from_file(file_path="function.js"))
 distribution = cloudfront.Distribution(
     stack,
     "Distribution",
@@ -41,5 +53,6 @@ distribution = cloudfront.Distribution(
     log_bucket=log_bucket,
     log_file_prefix="cloudfront",  # DatadogのログをCloudFrontのログとして扱うために、ログのプレフィックスを指定する
 )
+
 
 app.synth()
